16979b23d46f7dc74b44f33e7e523f455f96e4db
arshbhatti8 <arsh.bhatti8@gmail.com>
2017-05-16 19:08:55 -0700

    Add files via upload

--- newProject/routes/index.js
+++ newProject/routes/index.js
@@ -3,18 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin', { title: 'Administrator Page' });
+  res.render('login', { title: 'Administrator Page' });
 });
 // router.get('/', function(req, res, next) {
 //     res.render('moderator', { title: 'Moderator Page' });
 // });
 
 router.get('/moderator', function(req, res, next) {
+    req.session.type = "moderator";
     res.render('moderator', { title: 'Moderator Page' });
 });
 router.get('/profile', function(req, res, next) {
-    res.render('profile', { title: 'Profile' });
+    res.render('profile', { title: 'Profile',type:req.session.type});
 });
+router.get('/admin', function(req, res, next) {
+    req.session.type = "admin";
+    res.render('admin', { title: 'Login' });
+});
+
 // router.get('/', function(req, res, next) {
 //     res.render('moderator', { title: 'Moderator Page' });
 // });
